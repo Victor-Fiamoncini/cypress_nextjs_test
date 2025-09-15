089b6842833ede3be60f131a0acf748abacaa81d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-15 20:59:50 -0300

    feat: adding base project files

---
+++ src/app/components/todo-list/TodoList.js
@@ -0,0 +1,57 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function TodoList() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (event) => {
+    event.preventDefault()
+
+    if (!task.trim()) return
+
+    setTasks([...tasks, task])
+
+    setTask('')
+  }
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
+
+  return (
+    <div className="bg-gray-800 text-white p-5 max-w-md mx-auto rounded-lg mt-10">
+      <h2 className="text-xl font-bold mb-4">To-Do List</h2>
+
+      <form onSubmit={addTask} className="mb-4">
+        <input
+          className="text-gray-900 p-2 mr-2 rounded bg-white"
+          type="text"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+          placeholder="Add a new task"
+        />
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+          Add Task
+        </button>
+      </form>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className="flex justify-between items-center mb-2">
+            <span>{task}</span>
+
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded"
+              onClick={() => removeTask(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- src/app/settings/page.js
+++ src/app/settings/page.js
@@ -1,14 +1,18 @@
-export default function Home() {
+import TodoList from '@/app/components/todo-list/TodoList'
+
+export default function SettingsPage() {
   return (
     <main className="max-w-4xl mx-auto p-5">
-      <h2 data-id="home-header" className="text-3xl text-gray-800 dark:text-gray-200 font-bold mb-4">
-        Home
+      <h2 data-id="settings-header" className="text-3xl text-gray-800 dark:text-gray-200 font-bold mb-4">
+        Settings
       </h2>
 
-      <p className="text-gray-600 dark:text-gray-400 text-lg leading-relaxed">
+      <p className="text-gray-600 dark:text-gray-400 text-lg leading-relaxed m-10">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque doloremque delectus quis! Consectetur quae sint
         nesciunt dolor expedita nostrum, numquam amet. Molestias nemo veniam corrupti at dicta architecto accusamus a?
       </p>
+
+      <TodoList />
     </main>
   )
 }
