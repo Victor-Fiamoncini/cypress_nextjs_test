ff625ae99a886fbd22443e4a8eb555fdc073e227
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-15 22:37:22 -0300

    test: enhance sidebar component with data attributes for e2e testing & add e2e navigation tests

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,5 +1,34 @@
-describe('navigation tests', () => {
-  it('passes', () => {
+describe('navigation e2e tests', () => {
+  it('tests sidebar opening and closing buttons', () => {
     cy.visit('/')
+
+    cy.get('[data-id="open-sidebar"]').should('not.exist')
+    cy.get('[data-id="open-sidebar-btn"]').click()
+    cy.get('[data-id="open-sidebar"]').should('be.visible')
+    cy.get('[data-id="close-sidebar-btn"]').click()
+    cy.get('[data-id="open-sidebar"]').should('not.exist')
+  })
+
+  it('tests sidebar navigation links', () => {
+    cy.visit('/')
+
+    cy.get('[data-id="open-sidebar-btn"]').click()
+    cy.get('[data-id="about-link"]').click()
+    cy.get('[data-id="open-sidebar"]').should('not.exist')
+    cy.url().should('include', '/about')
+    cy.contains(/about me/i)
+
+    cy.visit('/')
+
+    cy.get('[data-id="open-sidebar-btn"]').click()
+    cy.get('[data-id="settings-link"]').click()
+    cy.get('[data-id="open-sidebar"]').should('not.exist')
+    cy.url().should('include', '/settings')
+    cy.contains(/settings/i)
+
+    cy.visit('/')
+
+    cy.url().should('include', '/')
+    cy.contains(/home/i)
   })
 })
--- src/app/components/sidebar/Sidebar.js
+++ src/app/components/sidebar/Sidebar.js
@@ -12,25 +12,41 @@ export default function Sidebar() {
 
   return (
     <div>
-      <button className="p-2 text-white bg-blue-500 hover:bg-blue-700" onClick={toggleSidebar}>
+      <button
+        data-id="open-sidebar-btn"
+        className="p-2 text-white bg-blue-500 hover:bg-blue-700"
+        onClick={toggleSidebar}
+      >
         ☰
       </button>
 
       {isOpen && (
-        <div className="fixed top-0 left-0 w-64 h-full bg-gray-800 text-white p-5 dark:bg-gray-900">
-          <button className="text-white bg-transparent hover:bg-gray-700 p-2 mb-4 rounded-lg" onClick={closeSidebar}>
+        <div
+          data-id="open-sidebar"
+          className="fixed top-0 left-0 w-64 h-full bg-gray-800 text-white p-5 dark:bg-gray-900"
+        >
+          <button
+            data-id="close-sidebar-btn"
+            className="text-white bg-transparent hover:bg-gray-700 p-2 mb-4 rounded-lg"
+            onClick={closeSidebar}
+          >
             X
           </button>
 
-          <Link href="/" className="block my-2 hover:text-blue-300" onClick={closeSidebar}>
+          <Link data-id="home-link" href="/" className="block my-2 hover:text-blue-300" onClick={closeSidebar}>
             Home
           </Link>
 
-          <Link href="/about" className="block my-2 hover:text-blue-300" onClick={closeSidebar}>
+          <Link data-id="about-link" href="/about" className="block my-2 hover:text-blue-300" onClick={closeSidebar}>
             About
           </Link>
 
-          <Link href="/settings" className="block my-2 hover:text-blue-300" onClick={closeSidebar}>
+          <Link
+            data-id="settings-link"
+            href="/settings"
+            className="block my-2 hover:text-blue-300"
+            onClick={closeSidebar}
+          >
             Settings
           </Link>
         </div>
