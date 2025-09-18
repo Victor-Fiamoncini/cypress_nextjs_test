50ed5a630b2436c9cb88c4a2ecc9947502219e99
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-17 23:02:48 -0300

    fix: correct DOM selectors and add cypress request interceptors for in TodoList tests

--- src/app/components/todo-list/TodoList.cy.js
+++ src/app/components/todo-list/TodoList.cy.js
@@ -3,6 +3,12 @@ import React from 'react'
 import TodoList from '@/app/components/todo-list/TodoList'
 
 describe('<TodoList />', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/todos', []).as('getTodos')
+    cy.intercept('POST', '/api/todos', { id: 1, text: 'Todo One' }).as('createTodo')
+    cy.intercept('DELETE', '/api/todos', { ok: true }).as('deleteTodo')
+  })
+
   it('renders', () => {
     cy.mount(<TodoList />)
 
@@ -15,7 +21,7 @@ describe('<TodoList />', () => {
     cy.get('[data-id="todo-list"]').children().should('have.length', 1)
     cy.contains(/todo one/i)
 
-    cy.get('[data-id="Todo One-remove-btn"]').click()
+    cy.get('[data-id="todo-1-remove-btn"]').click()
     cy.get('[data-id="todo-list"]').children().should('have.length', 0)
     cy.contains(/todo one/i).should('not.exist')
   })
